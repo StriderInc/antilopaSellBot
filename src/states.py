b71fd626b73d4bcbb8c0258522edd57c7b72fc59
Gleb Korotkiy <korotkiygs@yandex.com>
2025-05-28 01:09:04 +0300

    add states

---
+++ src/states.py
@@ -0,0 +1,21 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class FreeSaleStates(StatesGroup):
+    """Состояния для свободной продажи"""
+    waiting_service_name = State()
+    waiting_client_login = State()
+    waiting_comment = State()
+    waiting_amount = State()
+    confirmation = State()
+
+
+class OurProductStates(StatesGroup):
+    """Состояния для продажи нашего товара"""
+    waiting_game_name = State()
+    choosing_console = State()
+    choosing_position = State()
+    waiting_ps_login = State()
+    waiting_comment = State()
+    waiting_amount = State()
+    confirmation = State() 
